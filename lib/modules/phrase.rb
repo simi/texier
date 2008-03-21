117c25c8635df09b60fc6cd9dbd3b68d06280e49
adam.ciganek <adam.ciganek@787890ce-dd1f-0410-8805-67e76a918ff9>
2008-03-21 00:01:26 +0000

    - More phrases added to Phrase module

--- lib/modules/phrase.rb
+++ lib/modules/phrase.rb
@@ -6,15 +6,21 @@ module Texier::Modules
     inline_element('em-alt') {build_phrase('//', :em)}
     inline_element('strong') {build_phrase('**', :strong)}
     inline_element('strong+em') {build_phrase('***', [:strong, :em])}
-    # TODO: inline_element('quote') {build_phrase(['>>', '<<'], :q)}
+    inline_element('quote') {build_phrase(['>>', '<<'], :q)}
+    inline_element('code') {build_phrase('`', :code)}
     
     protected
     
-    def build_phrase(marker, names)
-      char = marker[0,1]
-      marker = expression(/#{Regexp.quote(marker)}(?!#{Regexp.quote(char)})/)
+    # Build expression that matches a phrase element.
+    def build_phrase(marks, names)
+      # Create expressions for opening and closing marks.
+      marks = [*marks].map do |mark|
+        expression(/#{Regexp.quote(mark)}(?!#{Regexp.quote(mark[0,1])})/)
+      end      
+      marks = [marks[0], marks[0]] if marks.size < 2
       
-      (marker & everything_up_to(marker) & marker).map do |_, content, _|
+      # Create phrase expression.
+      (marks[0] & everything_up_to(marks[1]) & marks[1]).map do |_, content, _|
         [*names].reverse.inject(content) do |element, name|
           Texier::Element.new(name, element)
         end
--- test/modules/phrase_test.rb
+++ test/modules/phrase_test.rb
@@ -10,7 +10,7 @@ class PhraseTest < Test::Unit::TestCase
   def test_emphasis_and_plain_text
     assert_output '<p>hello <em>world</em> again</p>', 'hello *world* again'
   end
-  
+
   def test_strong
     assert_output '<p><strong>hello world</strong></p>', '**hello world**'
   end
@@ -21,9 +21,13 @@ class PhraseTest < Test::Unit::TestCase
       '***hello***'
     )
   end
+
+  def test_quote
+    assert_output '<p><q>hello world</q></p>', '>>hello world<<'
+  end
   
-#  def test_quote
-#    assert_output '<p><q>hello world</q></p>', '>>hello world<<'
-#  end
+  def test_code
+    assert_output '<p><code>def test_code</code></p>', '`def test_code`'
+  end
   
 end
