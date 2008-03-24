25042ea34cdd663d5134f4c93b7321b9b295d646
adam.ciganek <adam.ciganek@787890ce-dd1f-0410-8805-67e76a918ff9>
2008-03-24 21:27:06 +0000

    - Modules::Modifier: 	- added support for class/id modifiers

--- lib/modules/modifier.rb
+++ lib/modules/modifier.rb
@@ -1,19 +1,40 @@
 require "#{File.dirname(__FILE__)}/../module"
 
 module Texier::Modules
+  # TODO: describe various modifiers
   class Modifier < Texier::Module
     # Modifier that does nothing.
     Empty = proc {|element| element}
     
     def initialize_parser(parser)
-      title = (e('(') & everything_up_to(')') & e(')')).map do |_, text, _|
+      # .(hello world)
+      title_modifier = (e('(') & everything_up_to(')') & e(')')).map do |_, value, _|
         proc do |element|
-          element[:title] = text
+          # TODO: if it is <img> (and possibly some other), use alt instead of
+          # title.
+          element[:title] = value
           element
         end
       end
       
-      modifier = discard(/ *\./) & title
+      # .[class #id]
+      class_modifier = (e('[') & everything_up_to(']') & e(']')).map do |_, values, _|
+        proc do |element|
+          classes = []
+          values.split(/ +/).each do |value|
+            if value[0] == ?#
+              element[:id] = value[1..-1]
+            else
+              classes << value
+            end
+          end
+          
+          element[:class] = classes.join(' ')
+          element
+        end
+      end
+      
+      modifier = discard(/ *\./) & (title_modifier | class_modifier)
       modifier = optional(modifier).map {|m| m || Empty}
       
       parser[:modifier] = modifier
--- test/modules/modifier_test.rb
+++ test/modules/modifier_test.rb
@@ -3,10 +3,30 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 # Test case for Texier::Modules::Modifier class
 class ModifierTest < Test::Unit::TestCase
   def test_modifier_should_be_optional
-    assert_output '<p><em>foo</em></p>', '*foo*'
+    assert_output '<p><em>hello</em></p>', '*hello*'
   end
   
   def test_title
-    assert_output '<p><em title="foo">bar</em></p>', '*bar .(foo)*'
+    assert_output '<p><em title="foo">hello</em></p>', '*hello .(foo)*'
+  end
+  
+  def test_class
+    assert_output '<p><em class="foo">hello</em></p>', '*hello .[foo]*'
+  end
+  
+  def test_many_classes
+    assert_output '<p><em class="foo bar">hello</em></p>', '*hello .[foo bar]*'
+    assert_output '<p><em class="foo bar">hello</em></p>', '*hello .[foo   bar]*'
+  end
+  
+  def test_id
+    assert_output '<p><em id="foo">hello</em></p>', '*hello .[#foo]*'
+  end
+  
+  def test_class_and_id
+    assert_output(
+      '<p><em class="foo" id="bar">hello</em></p>', 
+      '*hello .[foo #bar]*'
+    )
   end
 end
