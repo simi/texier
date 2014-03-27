59a9c241e2c9a0a0c1f9cafdfa419c22d08c0349
Josef Šimánek <josef.simanek@gmail.com>
2014-03-27 18:43:55 +0100

    Remove ugly rake fix and bump rake deps.

--- lib/rake_fix.rb
+++
@@ -1,7 +0,0 @@
-rake_version = Gem::Version.new(Rake::VERSION)
-rude_rake = Gem::Requirement.new('>=0.9.2')
-
-if rude_rake.satisfied_by?(rake_version) and respond_to?(:link, true)
-  undef :link
-end
-
--- lib/texier.rb
+++ lib/texier.rb
@@ -1,7 +1,1 @@
-begin
-  require 'rake_fix' if defined?(Rake)
-rescue
-  nil
-end
-
 require "texier/base"
